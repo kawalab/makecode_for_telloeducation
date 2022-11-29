ac2e60c40980884117b022280a23e5216ea337ee
Aoki-Yudai <52954197+Aoki-Yudai@users.noreply.github.com>
2022-11-29 15:32:20 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,45 +1,4 @@
-input.onGesture(Gesture.TiltRight, function () {
-    Tello.right(50)
-    basic.showLeds(`
-        . . # . .
-        . . . # .
-        # # # # #
-        . . . # .
-        . . # . .
-        `)
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    Tello.left(50)
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
-})
-input.onGesture(Gesture.LogoUp, function () {
-    Tello.back(50)
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        # . # . #
-        . # # # .
-        . . # . .
-        `)
-})
-input.onGesture(Gesture.LogoDown, function () {
-    Tello.forward(50)
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        # . # . #
-        . . # . .
-        . . # . .
-        `)
-})
 Tello.radiosetgroup(101)
-let distanse = 30
 basic.forever(function () {
     Tello.Setorder(5000)
 })
